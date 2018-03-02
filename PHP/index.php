ab819514d2c68a1412161b14e6b0721f16a4ede6
krzysiek <krzysiek@localhost.pi>
2018-03-02 22:17:57 +0100

    Update index.php to control manually

--- PHP/index.php
+++ PHP/index.php
@@ -1,26 +1,24 @@
 <?php 
 if (isset($_GET['led1'])) {	
 	if($_GET['led1'] == 1) {
-		// $python = `python3 /home/pi/pro/main.py`;
-		// echo $python;
-		$command = escapeshellcmd('/home/pi/pro/main.py');
+		$command = escapeshellcmd('/home/pi/testy/example.py');
 		$output = shell_exec($command);
 		echo $output;
-		
-		print("led 1 has been activ4ted");
 	} else {
-		print("led1 has not been activated");
+		$command = escapeshellcmd('touch /home/pi/testy/USUNTO.py');
+		$output = shell_exec($command);
+		echo $output;
 	}
 }
 if(isset($_GET['led2'])) {
 	if($_GET['led2'] == 1) {
-		$command = escapeshellcmd('/home/pi/pro/main.py');
+		$command = escapeshellcmd('/home/pi/skrypt_manual_on.py');
 		$output = shell_exec($command);
 		echo $output;
-
-		print("led 2 has been activated biacz");
 	} else {
-		print("led 2 is really off");
+		$command = escapeshellcmd('/home/pi/skrypt_manual_off.py');
+		$output = shell_exec($command);
+		echo $output;
 	}
 }
 ?>
