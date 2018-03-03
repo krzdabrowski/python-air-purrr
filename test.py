2d35c6447f4b3708e7c3e22ae549eead00d8ee0f
krzysiek <krzysiek@localhost.pi>
2018-03-03 13:40:32 +0100

    Change state in workstate.txt while CTRL+C

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ def printValues(timing, values, unit_of_measure):
     return 
 
 
-sensor = SDS011("/dev/ttyUSB0", timeout=5, unit_of_measure=SDS011.UnitsOfMeasure.MassConcentrationEuropean)
+sensor = SDS011("/dev/ttyUSB0", timeout=1, unit_of_measure=SDS011.UnitsOfMeasure.MassConcentrationEuropean)
          
 
 if __name__ == "__main__":            
@@ -44,6 +44,8 @@ if __name__ == "__main__":
     while True:
         try:  # pojedynczy tryb rozgrzewania (okolo 30 sek)
             sensor.workstate = SDS011.WorkStates.Measuring
+            with open('/var/www/html/workstate.txt', 'w') as f_workstate:
+                print(sensor.workstate, file=f_workstate)
             sensor.dutycycle = 1
             last = time.time()
             while True:  # pojedyncze pingowanie podczas rozgrzewania (= timeout = 5 sek)
@@ -51,16 +53,20 @@ if __name__ == "__main__":
                 values = sensor.get_values()
                 if values is not None:
                     printValues(time.time() - last, values, sensor.unit_of_measure)
-                    with open('datas.txt', 'w') as f_output:
-                        print(str(values[1]) + '\n' + str(values[0]), file=f_output)
+                    with open('data.txt', 'w') as f_data:
+                        print(str(values[1]) + '\n' + str(values[0]), file=f_data)
                     break
                 print("Waited %d secs, no values read, we try again" % (time.time() - last1))
                 
-            sensor.workstate = SDS011.WorkStates.Sleeping                
+            sensor.workstate = SDS011.WorkStates.Sleeping
+            with open('/var/www/html/workstate.txt', 'w') as f_workstate:
+                print(sensor.workstate, file=f_workstate)
             print("Read was successful. Going to sleep for 15 minutes")
             time.sleep(900)
 
         except KeyboardInterrupt:
             sensor.reset()
+            with open('/var/www/html/workstate.txt', 'w') as f_workstate:
+                print(sensor.workstate, file=f_workstate)
             sensor = None
             sys.exit("Sensor reset due to a KeyboardInterrupt")
