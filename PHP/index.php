9cad3069903150b8b9293b5dafa5dbf1cdb8e0a3
krzysztof_rpi <krzdabrowski95@gmail.com>
2018-03-21 18:32:45 +0100

    Add basic auth support on Apache2 server

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,17 @@
-<?php 
-if (isset($_GET['AUTO'])) {	
-	if($_GET['AUTO'] == 1) {
+<?php
+
+session_start();
+read_file();
+
+function login() {
+    header("WWW-Authenticate: Basic"); 
+    header("HTTP/1.0 401 Unauthorized");
+    die; 
+}
+
+function work() {
+    if ($_POST) {	
+	if($_POST["req"] == "MANUAL=1" || $_POST["req"] == "AUTO=1") {
 		$command = escapeshellcmd('/home/pi/skrypt_on.py');
 		$output = shell_exec($command);
 		echo $output;
@@ -9,16 +20,33 @@ if (isset($_GET['AUTO'])) {
 		$output = shell_exec($command);
 		echo $output;
 	}
+    }
 }
-if(isset($_GET['MANUAL'])) {
-	if($_GET['MANUAL'] == 1) {
-		$command = escapeshellcmd('/home/pi/skrypt_on.py');
-		$output = shell_exec($command);
-		echo $output;
-	} else {
-		$command = escapeshellcmd('/home/pi/skrypt_off.py');
-		$output = shell_exec($command);
-		echo $output;
-	}
+
+function read_file() {
+    $data = explode("\n", file_get_contents('/home/pi/Desktop/db.txt'));
+    return $data;
 }
+
+
+// auth
+if (!isset($_SESSION["AUTH_SUCCESS"])) {
+    $_SESSION["AUTH_SUCCESS"] = 0;
+}
+if ($_SESSION["AUTH_SUCCESS"] == 0) {
+    $user = read_file()[0]; 
+    $pass = read_file()[1];
+
+    if ($_SERVER["PHP_AUTH_USER"] != $user || $_SERVER["PHP_AUTH_PW"] != $pass) { 
+        login();
+    } else {
+        $_SESSION["AUTH_SUCCESS"] = 1;
+	work();
+        echo "gut";
+    }
+}
+if ($_SESSION["AUTH_SUCCESS"] == 0) {
+    die("You have entered a wrong password/username.");
+}
+
 ?>
