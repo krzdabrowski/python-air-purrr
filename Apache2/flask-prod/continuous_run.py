497c018a89432d60422eacc4c4f3291a7dce6883
krzdabrowski <krz.dabrowski95@gmail.com>
2019-03-17 19:20:40 +0100

    implement json and 1 endpoint instead of 2

--- Apache2/flask-prod/continuous_run.py
+++ Apache2/flask-prod/continuous_run.py
@@ -20,6 +20,7 @@ GNU General Public License for more details.
 import sys
 import os
 import time
+import json
 import paho.mqtt.publish as publish
 from sds011 import SDS011
 from timeout import timeout
@@ -28,6 +29,8 @@ from timeout import timeout
 # create instance of sensor & clear it
 sensor = SDS011("/dev/ttyUSB0", timeout=1, unit_of_measure=SDS011.UnitsOfMeasure.MassConcentrationEuropean)
 sensor.reset()     
+
+json_data = {}
 
 # values need for ThingSpeak
 channel_ID = "462987" # your channel_ID
@@ -68,11 +71,14 @@ if __name__ == "__main__":
 
     while True:
         try:
+
             # warm-up mode
             sensor.workstate = SDS011.WorkStates.Measuring
             # update workstate for an app
-            with open('/var/www/html/workstate.txt', 'w') as f_workstate:
-                print(sensor.workstate, file=f_workstate)
+            with open('/var/www/html/data.json', 'w') as file_obj:
+                json_data['workstate'] = str(sensor.workstate)
+                json.dump(json_data, file_obj)
+                # print(sensor.workstate, file=f_workstate)
             sensor.dutycycle = 1
             last = time.time()
             
@@ -83,16 +89,22 @@ if __name__ == "__main__":
                 if values is not None:
                     print_values(time.time() - last, values, sensor.unit_of_measure)
                     # make them online
-                    with open('/var/www/html/pm_data.txt', 'w') as f_data:
-                        print(str(values[1]) + '\n' + str(values[0]), file=f_data)
+                    with open('/var/www/html/data.json', 'w') as file_obj:
+                        json_data['values'] = {}
+                        json_data['values']['pm25'] = values[1]
+                        json_data['values']['pm10'] = values[0]
+                        json.dump(json_data, file_obj)
+                        # print(str(values[1]) + '\n' + str(values[0]), file=f_data)
                     break
                 print("Waited {:.0f} secs, no values read, trying again".format(time.time() - last1))
             
             # sleeping mode
             sensor.workstate = SDS011.WorkStates.Sleeping
             # update workstate for an app
-            with open('/var/www/html/workstate.txt', 'w') as f_workstate:
-                print(sensor.workstate, file=f_workstate)
+            with open('/var/www/html/data.json', 'w') as file_obj:
+                json_data['workstate'] = str(sensor.workstate)
+                json.dump(json_data, file_obj)
+                # print(sensor.workstate, file=f_workstate)
             # send data to ThingSpeak
             payload = "field1=" + str(values[1]) + "&amp;amp;field2=" + str(values[0])
             try:
@@ -106,8 +118,10 @@ if __name__ == "__main__":
 
         except KeyboardInterrupt:
             sensor.reset()
-            with open('/var/www/html/workstate.txt', 'w') as f_workstate:
-                print(sensor.workstate, file=f_workstate)
+            with open('/var/www/html/data.json', 'w') as file_obj:
+                json_data['workstate'] = str(sensor.workstate)
+                json.dump(json_data, file_obj)
+                # print(sensor.workstate, file=file_obj)
             sensor = None
             sys.exit("Sensor reset due to a KeyboardInterrupt")
 
