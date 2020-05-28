29defc45afe630c6d3fc85c8da3ae7a9aeb214ae
Krzysztof Dąbrowski <36383670+krzdabrowski@users.noreply.github.com>
2020-05-28 16:44:52 +0200

    Revert temporary fixes

--- backend/main.py
+++ backend/main.py
@@ -130,11 +130,15 @@ if __name__ == '__main__':
         .reshape(-1, 1)
     Y_pm25 = dataframe.pm25.values
     Y_pm10 = dataframe.pm10.values
-   
-    nonlinear_hyperparameters_tuning(X_daily, Y_pm25, Y_pm10)
-    xgboost_hyperparameters_tuning(X_daily, Y_pm25, Y_pm10)
+    
+    # print('##### HYPERPARAMETERS TUNING #####')
+    
+    # nonlinear_hyperparameters_tuning(X_daily, Y_pm25, Y_pm10)
+    # xgboost_hyperparameters_tuning(X_daily, Y_pm25, Y_pm10)
 
-    while True:   
+    while True:
+        print('\n\n##### CALCULATING PREDICTIONS #####')
+    
         forecast_results.linear = linear_regression(X_daily, Y_pm25, Y_pm10)
         forecast_results.nonlinear = nonlinear_regression(X_daily, Y_pm25, Y_pm10)
         forecast_results.xgboost = xgboost_regression(X_daily, Y_pm25, Y_pm10)
